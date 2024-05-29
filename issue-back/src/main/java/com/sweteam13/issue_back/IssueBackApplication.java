7088bae49d3368d789bdec3e26a78e247318a636
Chrysanthemum3552 <chrysanthemum3552@gmail.com>
2024-05-30 01:33:07 +0900

    기본틀 구축

---
+++ issue-back/src/main/java/com/sweteam13/issue_back/IssueBackApplication.java
@@ -0,0 +1,13 @@
+package com.sweteam13.issue_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueBackApplication.class, args);
+	}
+
+}
---
+++ issue-back/src/test/java/com/sweteam13/issue_back/IssueBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweteam13.issue_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
