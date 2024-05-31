3e8be8333a5fb870fc3c668cf6a80270bc25bf3c
Jacob <chrysanthemum3552@gmail.com>
2024-05-31 15:55:15 +0900

    Footer 작성롼료

--- issue-front/src/App.tsx
+++ issue-front/src/App.tsx
@@ -1,41 +1,11 @@
 import './App.css';
-import BoardItem from 'components/BoardItem';
-import CommentItem from 'components/CommentItem';
-import type { BoardListItem as BoardListItemType } from 'types/interface';
-import type { CommentListItem as ConmmentListItemType} from 'types/interface';
-import { commentListMock } from 'mocks';
-import InputBox from 'components/InputBox';
-import { useState } from 'react';
-
-
-const exampleCommentListItem: ConmmentListItemType = {
-  name: 'test1',
-  writeDatetime: '1시간전',
-  content: '수정요청'
-}
-const exampleBoardListItem: BoardListItemType = {
-  issue_no: 1,
-  title: 'Example Title',
-  content: 'This is an example content for the board list item.',
-  commentCount: 5,
-  viewCount: 150,
-  writeDatetime: '2024-05-29 14:30:00',
-  writerName: 'John Doe',
-  status: 'new',
-  assignee: 'admin',
-  fixer: 'admin',
-  type: 'front-end',
-  priority: 'major',
-  name: 'test1'
-};
-
+import Footer from 'layouts/Footer';
 function App() {
 
-  const [value, setValue] = useState<string>('');
 
   return (
     <>
-      <InputBox label='이름' type='text' placeholder='이름을 입력하세요' value={value} error={true} setValue={setValue} message='aaaa'/>
+      <Footer />
     </>
   );
 }
--- issue-front/src/layouts/Footer/index.tsx
+++ issue-front/src/layouts/Footer/index.tsx
@@ -3,20 +3,23 @@ import './style.css'
 
 
 export default function footer() {
+
+  
+
+
   return (
     <div id='footer'>
         <div className='footer-container'>
             <div className='footer-top'>
                 <div className='footer-logo-box'>
                     <div className='icon-box'>
-                        <div className='logo-light-icon'></div>
+                        <div className='icon logo-light-icon'></div>
                     </div>
-                    <div className='footer-logo-text'></div>
+                    <div className='footer-logo-text'>{'텍스트'}</div>
                 </div>
-                <div className='footer-link-box'>
-                    <div className='footer-email-link'></div>
-                    <div className='icon-button'></div>
-                </div>
+            </div>
+            <div className='footer-bottom'>
+                <div className='fotter-copyright'>{'여기에도 목록표시'}</div>
             </div>
         </div>
     </div>
